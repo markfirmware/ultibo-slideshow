0631dcc3dd3108cb2e7b79be5f4f851468ff05d4
markfirmware <markfirmware@users.noreply.github.com>
2016-12-09 07:06:25 -0500

    imagemagick

--- scripts/appveyor-install-imagemagick.tool.sh
+++ scripts/appveyor-install-imagemagick.tool.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-ls -lt /c/ProgramData/chocolatey
+ls -lt /c/ProgramData/chocolatey/lib
 
-if [[ ! -e /c/ProgramData/chocolatey ]]
+if [[ ! -e /c/ProgramData/chocolatey/lib/imagemagick.tool ]]
 then
     set -x
     appveyor AddMessage "installing chocolatey imagemagick.tool"
     choco install imagemagick.tool
+    ls -lt /c/ProgramData/chocolatey/lib
 fi
