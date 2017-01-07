897064407600f10b3425e0aa950769c51af78122
markfirmware <markfirmware@users.noreply.github.com>
2017-01-07 16:40:51 -0500

    run-qemu script

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-rm *.elf *.img *.o *.ppu
+rm -f i*.bin *.elf *.img *.o *.ppu
 docker run --rm -v $(pwd):/workdir markfirmware/ufpc \
  -dTARGET_QEMU32 -B -Tultibo -O2 -Parm -CpARMV7A -WpQEMUVPB @/root/ultibo/core/fpc/bin/qemuvpb.cfg \
  pslideshow.lpr
