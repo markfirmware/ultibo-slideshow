480ac731ec4665c56371c5b92f60b9568991b948
markfirmware <markfirmware@users.noreply.github.com>
2017-01-09 02:31:12 -0500

    qemu default framebuffer 800x600

--- scripts/linux-test-with-qemu-monitor-commands.sh
+++ scripts/linux-test-with-qemu-monitor-commands.sh
@@ -2,7 +2,7 @@
 
 sleep 1
 echo -en \\001c
-echo screendump output/screen-01.ppm
+echo screendump build-test-output/screen-01.ppm
 sleep 7
-echo screendump output/screen-02.ppm
+echo screendump build-test-output/screen-02.ppm
 echo quit
--- scripts/linux-test-with-qemu.sh
+++ scripts/linux-test-with-qemu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-mkdir -p output
+mkdir -p build-test-output
 
 ./scripts/linux-test-with-qemu-monitor-commands.sh | \
  qemu-system-arm \
@@ -11,11 +11,11 @@ mkdir -p output
  -m 256M \
  -usb \
  -display none \
- -serial mon:stdio |& tee output/raw.log | egrep -iv '^(alsa|pulseaudio:|audio:)' > output/serial.log
+ -serial mon:stdio |& tee build-test-output/raw.log | egrep -iv '^(alsa|pulseaudio:|audio:)' > build-test-output/serial.log
 
-convert output/screen-01.ppm output/screen-01.png
-convert output/screen-02.ppm output/screen-02.png
-rm output/*.ppm
+convert build-test-output/screen-01.ppm build-test-output/screen-01.png
+convert build-test-output/screen-02.ppm build-test-output/screen-02.png
+rm build-test-output/*.ppm
 
-cat output/serial.log
-ls output
+cat build-test-output/serial.log
+file build-test-output/*
